b438100d29365000135b4140cf41ecdd233d659e
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-11-25 14:08:40 -0400

    Bootcamp Coding The Future Avanade: Programação em C# com CRM Dynamics

---
+++ PrimeirosPassosLinguagemCSharp/Arrays_Listas/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] arrayInteiros = new int[3];
+
+
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 50;
+
+//Array.Resize(ref arrayInteiros,arrayInteiros.Length *2);
+
+
+
+int[] arrayInteirosDobrado = new int[arrayInteiros.Length  * 2];
+Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
+
+Console.WriteLine("Percorrendo com o FOR");
+
+for (int i = 0; i < arrayInteiros.Length; i++)
+{
+    Console.WriteLine($"Posição Nº{i} - {arrayInteiros[i]}");
+}
+
+
+/*Console.WriteLine("Percorrendo com o FOREACH");
+int contadorForeach = 0;
+foreach (int item in arrayInteiros)
+{
+    Console.WriteLine($"Posição Nº {contadorForeach}- {item}");
+    contadorForeach++;
+
+}*/
